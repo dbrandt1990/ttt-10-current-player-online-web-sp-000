751d0ba8d020d51bcd1f0c907ba86fc3e1408351
Danny Brandt <dvbrandt90@gmail.com>
2020-12-01 06:28:30 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,7 +8,7 @@ end
 def turn_count(board)
   turns = 0
   board.each do |space|
- space == "X" || space == "O" ? turns += 1 : continue
+ space == "X" || space == "O" ? turns += 1 : next
   end
   return turns
 end
